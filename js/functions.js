4c8c9126a72fd6dd5b0636f103518fc895f25454
Dkey51 <oniblekir@gmail.com>
2023-09-13 22:10:48 +0300

    Функция поиска чисел

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,24 @@
-
+// Функция проверки на палиндром с разными
 function checkPalindrom (str) {
   return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
 }
 
 checkPalindrom("довОд");
+
+
+//Функция поиска чисел
+function checkNum(str) {
+  if (typeof str === 'number') {
+    return str;
+  }
+
+  const numArr = str?.match(/\d+/g);
+
+  if (!numArr) {
+    return NaN;
+  }
+
+  return numArr.map(elem => parseInt(elem)).join('');
+}
+console.log(checkNum('fdgdfgd456fgdfg'));
 
