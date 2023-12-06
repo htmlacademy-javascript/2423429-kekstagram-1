5d9cf56344be13c364793e9735ebfdda5de3035b
Dkey51 <oniblekir@gmail.com>
2023-12-06 15:49:16 +0300

    удалил лишнюю строку из main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
 import { similarPost } from './util.js';
 import { renderPicture } from './render-picture.js';
-
-import { renderBigPicture } from'./render-big-pucture.js';
-
 renderPicture(similarPost);
