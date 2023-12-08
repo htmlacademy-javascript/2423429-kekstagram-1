2f4bda29ad9f687be7c862f4468bd3bfb7befb31
Dkey51 <oniblekir@gmail.com>
2023-12-08 16:32:51 +0300

    задание 8.15 на проверку

--- js/render-big-pucture.js
+++ js/render-big-pucture.js
@@ -8,30 +8,41 @@ export function renderBigPicture(pictureElem, Post) {
     document.body.classList.add('.modal-open');
     bigPicrute.querySelector('.big-picture__img img').src = Post.avatar;
     bigPicrute.querySelector('.likes-count').textContent = Post.likes;
-    //TODO: заменить Post.message на Post.comments
     bigPicrute.querySelector('.social__caption').textContent = Post.description;
+    bigPicrute.querySelector('.comments-count').textContent = Post.comments.length;
     //bigPicrute.querySelector('.social__comment-count').classList.add('hidden');
     //bigPicrute.querySelector('.social__comments-loader').classList.add('hidden');
     //отобразил
-    // const buttonAddComment = bigPicrute.querySelector('.social__comments-loader');
-    // buttonAddComment.addEventListener('click', () =>{
-    // const hiddenComments = listComment.querySelectorAll('.social__comment.hidden');
-    // });
+    const buttonAddComment = bigPicrute.querySelector('.social__comments-loader');
+    buttonAddComment.addEventListener('click', () =>{
+      const hiddenComments = listComment.querySelectorAll('.social__comment.hidden');
+      hiddenComments.forEach((comment, i) =>{
+        if(i < 5){
+          comment.classList.remove('hidden');
+        }
+      });
+      const comments = bigPicrute.querySelectorAll('.social__comment');
+      let counter = 0;
+      comments.forEach((comment)=> {
+        if(!comment.classList.contains('.hidden')){
+          counter++;
+        }
+      });
+      // let comments = visibleComments.filter((comment) => !comment.classList.contains('hidden')).length;
+      bigPicrute.querySelector('.comments-count-visible').textContent = counter;
+    });
     //кнопка добавить еще, нужно доработать
     for(let i = 0; i < Post.comments.length; i++){
       const thisComment = commentTemplate.cloneNode(true);
       thisComment.querySelector('.social__picture').src = Post.comments[i].avatar;
       thisComment.querySelector('.social__picture').alt = Post.comments[i].name;
       thisComment.querySelector('.social__text').textContent = Post.comments[i].message;
-      listComment.appendChild(thisComment);
-    }
-    const commentsArray = listComment.querySelectorAll('.social__comment');
-    bigPicrute.querySelector('.comments-count').textContent = commentsArray.length;
-    commentsArray.forEach((comment,i) => {
-      if(i <= 4){
-        comment.classList.add('hidden');
+      if(i >= 5){
+        thisComment.querySelector('.social__comment').classList.add('hidden');
       }
-    });
+      listComment.appendChild(thisComment);
+
+    }
   };
   const bigPicruteClose = () => {
     bigPicrute.classList.add('hidden');
--- js/util.js
+++ js/util.js
@@ -34,7 +34,7 @@ const generatePost = () => {
     id,
     avatar: `photos/${id}.jpg`,
     likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
-    comments: Array.from({length: 10}, generateComment), //TODO:переименовать message в comments
+    comments: Array.from({length: 10}, generateComment),
     name: getRandomArrayElement(RANDOM_NAMES),
     description: 'Привет описание!'
   };
