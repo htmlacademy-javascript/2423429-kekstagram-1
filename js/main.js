d9ea474a2bdd7e300659e0e5ef112c9ae73051a0
Dkey51 <oniblekir@gmail.com>
2024-01-15 20:53:27 +0300

    добавил регулярное выражение для hashtag

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import { similarPost } from './util.js';
 import { renderPicture } from './render-picture.js';
 renderPicture(similarPost);
+import {} from './workForm.js';
---
+++ js/workForm.js
@@ -0,0 +1,2 @@
+const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
+hashtag.test('#keks');
