55158c4169e64375ff3c830e039980d65a3dace5
Dkey51 <oniblekir@gmail.com>
2024-01-30 14:03:29 +0300

    надо разобрать подробнее Ptistine

--- js/work-form.js
+++ js/work-form.js
@@ -1,6 +1,27 @@
+
 const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
 
-//
+ const orderForm = document.querySelector('.form');
+console.log(orderForm);
+
+// const pristine = new Pristine(orderForm, {
+//   classTo: 'form__item',
+//   errorClass: 'form__item--invalid',
+//   successClass: 'form__item--valid',
+//   errorTextParent: 'form__item',
+//   errorTextTag: 'span',
+//   errorTextClass: 'form__error'
+// });
+
+
+// pristine.addValidator(orderForm.querySelector('##upload-file'));
+
+
+// orderForm.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//   pristine.validate();
+// });
+
 
 const editPhoto = document.querySelector('.img-upload__overlay');
 
@@ -17,6 +38,4 @@ closeUploadButton.addEventListener('click', ()=>{
 const uploadPicture = document.querySelector('.img-upload__preview img');
 const Inputt = document.querySelector('.img-upload__start');
 const setPicture = Inputt.querySelector('.img-upload__input').textContent;
-console.log(uploadPicture);
-console.log(setPicture);
 
