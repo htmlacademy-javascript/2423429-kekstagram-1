485e5a736abc916e2b0073bb9244da10d7889068
Dkey51 <oniblekir@gmail.com>
2024-02-11 21:01:01 +0300

    причесали валижацию формы

--- js/work-form.js
+++ js/work-form.js
@@ -1,12 +1,9 @@
-
 const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
-
 const orderForm = document.querySelector('.img-upload__overlay');
-
 const pristine = new Pristine(orderForm, {
   classTo: 'img-upload__form',
-  errorClass: 'error',
-  successClass: 'success',
+  errorClass: 'pristin_error',
+  successClass: 'pristin_success',
   errorTextParent: 'img-upload__wrapper',
   errorTextTag: 'span',
   errorTextClass: 'form__error'
@@ -14,39 +11,29 @@ const pristine = new Pristine(orderForm, {
 
 pristine.addValidator(orderForm.querySelector('.text__hashtags'), (value) =>{
   const hashtagString = value;
-  const hashtagArray = hashtagString.split(' ');
-  const testHashtag = function () {
+  const hashtagArray = hashtagString.split(' ').map((thisHashtag) => thisHashtag.toLowerCase());
+  if (hashtagArray.length > 5){
+    return false;
+  }
+  return true;
+}, 'не больше 5 хэштегов');
 
-    function hasDuplicates() {
-      const valuesSoFar = Object.create(null);
-      console.log(valuesSoFar,'сейчас в объекте');
-      for (let i = 0; i < hashtagArray.length; i++) {
-        const thisHashtag = hashtagArray[i];
-        if (thisHashtag in valuesSoFar) {
-          return console.log(false, 'повторяется');
-        }
-        valuesSoFar[thisHashtag] = true;
-      }
-      return console.log(true, 'не повторяется');
+pristine.addValidator(orderForm.querySelector('.text__hashtags'), (value) =>{
+  const hashtagString = value;
+  const hashtagArray = hashtagString.split(' ').map((thisHashtag) => thisHashtag.toLowerCase());
+  const values = {};
+  for (let i = 0; i < hashtagArray.length; i++) {
+    const thisHashtag = hashtagArray[i];
+    if (thisHashtag in values) {
+      return false;
     }
-    hasDuplicates(hashtagArray);
-    console.log('количество хэштегов' , hashtagArray.length);
-  };
-  testHashtag(hashtagArray);
-
-  if (hashtagArray.length > 7){
-    return console.log(false, 'больше 7 элементов');
+    if(!hashtag.test(thisHashtag)){
+      return false;
+    }
+    values[thisHashtag] = true;
   }
-
-  //TODO: должен быть код, который проверяет валидацию хештега
-
-  if (hashtag.test(value)){
-    return true;
-  }
-  return false;
-}, 'unValid');
-//TODO: 140 символов для коментария лимит, или через html атрибуты или через валидатор по аналогии выше
-
+  return true;
+}, 'хэштеги должны быть уникальными, начинаться с #, длинна не больше 20 символов');
 orderForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
@@ -54,17 +41,13 @@ orderForm.addEventListener('submit', (evt) => {
 
 const editPhoto = document.querySelector('.img-upload__overlay');
 const changePhoto = document.querySelector('.img-upload__input');
-
 changePhoto.addEventListener('change', () =>{
   editPhoto.classList.remove('hidden');
   document.body.classList.add('modal-open');
-
 });
 
 const closeUploadButton = editPhoto.querySelector('.img-upload__cancel');
-
 closeUploadButton.addEventListener('click', ()=>{
   editPhoto.classList.add('hidden');
   document.body.classList.remove('modal-open');
 });
-
