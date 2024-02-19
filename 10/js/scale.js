451cd518903c02e9c8d623dc5cccc7b02a9f36f2
Keks <keks@htmlacademy.ru>
2024-02-19 18:10:52 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/scale.js
@@ -0,0 +1,48 @@
+const STEP = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+const SCALE_DEVIDER = 100;
+const buttonScaleSmaller = document.querySelector('.scale__control--smaller');
+const buttonScaleBigger = document.querySelector('.scale__control--bigger');
+const scaleControl = document.querySelector('.scale__control--value');
+const imgUploadPreview = document.querySelector('.img-upload__preview img');
+
+export const scalePicture = () => {
+  buttonScaleBigger.disabled = true;
+  //напишите код который позволит пользователю редактировать масштаб изображения
+  const clickSmaller = () => {
+    let thisScale = parseInt(scaleControl.value, 10);
+    buttonScaleBigger.disabled = false;
+    thisScale -= STEP;
+    scaleControl.value = thisScale;
+    scaleControl.value = `${thisScale}%`;
+    if(thisScale <= MIN_SCALE){
+      buttonScaleSmaller.disabled = true;
+    } else {
+      buttonScaleSmaller.disabled = false;
+    }
+    imgUploadPreview.style.transform = `scale(${thisScale / SCALE_DEVIDER})`;
+  };
+  buttonScaleSmaller.addEventListener('click', clickSmaller);
+
+  const clickBigger = () => {
+    let thisScale = parseInt(scaleControl.value, 10);
+    buttonScaleSmaller.disabled = false;
+    thisScale += STEP;
+    scaleControl.value = thisScale;
+    scaleControl.value = `${thisScale}%`;
+    if (thisScale >= MAX_SCALE) {
+      buttonScaleBigger.disabled = true;
+    } else {
+      buttonScaleBigger.disabled = false;
+    }
+    imgUploadPreview.style.transform = `scale(${thisScale / SCALE_DEVIDER})`;
+  };
+  buttonScaleBigger.addEventListener('click', clickBigger);
+};
+
+export const resetScale = () => {
+  scaleControl.value = '100%';
+  imgUploadPreview.style.transform = 'scale(1)';
+
+};
---
+++ 10/js/work-form.js
@@ -0,0 +1,73 @@
+import { scalePicture, resetScale, } from './scale.js';
+import { setDefaultEffect } from './effects.js';
+const editPhoto = document.querySelector('.img-upload__overlay');
+const closeUploadButton = editPhoto.querySelector('.img-upload__cancel');
+const changePhoto = document.querySelector('.img-upload__input');
+const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
+const orderForm = document.querySelector('.img-upload__overlay');
+const pristine = new Pristine(orderForm, {
+  classTo: 'img-upload__form',
+  errorClass: 'pristin_error',
+  successClass: 'pristin_success',
+  errorTextParent: 'img-upload__wrapper',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+});
+
+pristine.addValidator(orderForm.querySelector('.text__hashtags'), (value) =>{
+  const hashtagString = value;
+  const hashtagArray = hashtagString.split(' ').map((thisHashtag) => thisHashtag.toLowerCase());
+  if (hashtagArray.length > 5){
+    return false;
+  }
+  return true;
+}, 'не больше 5 хэштегов');
+
+pristine.addValidator(orderForm.querySelector('.text__hashtags'), (value) =>{
+  const hashtagString = value;
+  const hashtagArray = hashtagString.split(' ').map((thisHashtag) => thisHashtag.toLowerCase());
+  const values = {};
+  for (let i = 0; i < hashtagArray.length; i++) {
+    const thisHashtag = hashtagArray[i];
+    if (thisHashtag in values) {
+      return false;
+    }
+    if(!hashtag.test(thisHashtag)){
+      return false;
+    }
+    values[thisHashtag] = true;
+  }
+  return true;
+}, 'хэштеги должны быть уникальными, начинаться с #, длинна не больше 20 символов');
+orderForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
+changePhoto.addEventListener('change', () =>{
+  editPhoto.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+});
+const closeForm = ()=>{
+  editPhoto.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  resetScale();
+  setDefaultEffect();
+};
+closeUploadButton.addEventListener('click', closeForm);
+document.addEventListener('keydown', (evt)=>{
+  if(evt.key === 'Escape') {
+    closeForm();
+  }
+});
+const setComment = document.querySelector('.text__description');
+
+setComment.addEventListener('keydown', (evt)=>{
+  if(evt.key === 'Escape') {
+    setComment.blur();
+    evt.stopPropagation();
+  }
+});
+scalePicture();
+
+
