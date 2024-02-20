fc1473d74d4dd01d982ee6b00afce63d6cbab82c
Dkey51 <oniblekir@gmail.com>
2024-02-20 11:23:33 +0300

    переделал функцию scalePicture, нужно проверить

--- js/scale.js
+++ js/scale.js
@@ -3,43 +3,33 @@ const buttonScaleSmaller = document.querySelector('.scale__control--smaller');
 const buttonScaleBigger = document.querySelector('.scale__control--bigger');
 const scaleControl = document.querySelector('.scale__control--value');
 const imgUploadPreview = document.querySelector('.img-upload__preview img');
+let thisScale = parseInt(scaleControl.value, 10);
 
-export const scalePicture = () => {
-  buttonScaleBigger.disabled = true;
-  //напишите код который позволит пользователю редактировать масштаб изображения
-  //TODO: постараться сделать 1 функцией
-  const clickSmaller = () => {
-    let thisScale = parseInt(scaleControl.value, 10);
-    buttonScaleBigger.disabled = false;
+const makeScaleClickHandler = (isNegative, scaleThreshold) => function () {
+  buttonScaleBigger.disabled = false;
+  if (isNegative) {
     thisScale -= STEP;
-    scaleControl.value = thisScale;
-    scaleControl.value = `${thisScale}%`;
-    if(thisScale <= MIN_SCALE){
-      buttonScaleSmaller.disabled = true;
-    } else {
-      buttonScaleSmaller.disabled = false;
-    }
-    imgUploadPreview.style.transform = `scale(${thisScale / SCALE_DEVIDER})`;
-  };
-  buttonScaleSmaller.addEventListener('click', clickSmaller);
-
-  const clickBigger = () => {
-    let thisScale = parseInt(scaleControl.value, 10);
-    buttonScaleSmaller.disabled = false;
+  } else {
     thisScale += STEP;
-    scaleControl.value = thisScale;
-    scaleControl.value = `${thisScale}%`;
-    if (thisScale >= MAX_SCALE) {
-      buttonScaleBigger.disabled = true;
-    } else {
-      buttonScaleBigger.disabled = false;
-    }
-    imgUploadPreview.style.transform = `scale(${thisScale / SCALE_DEVIDER})`;
-  };
-  buttonScaleBigger.addEventListener('click', clickBigger);
+  }
+  scaleControl.value = thisScale;
+  scaleControl.value = `${thisScale}%`;
+  if(thisScale === scaleThreshold){
+    this.disabled = true;
+  } else {
+    this.disabled = false;
+  }
+  imgUploadPreview.style.transform = `scale(${thisScale / SCALE_DEVIDER})`;
 };
 
+export function scalePicture () {
+  buttonScaleSmaller.addEventListener('click', makeScaleClickHandler(true, MIN_SCALE));
+  buttonScaleBigger.addEventListener('click', makeScaleClickHandler(false, MAX_SCALE));
+}
+
 export const resetScale = () => {
+  buttonScaleBigger.disabled = true;
+  buttonScaleSmaller.disabled = false;
   scaleControl.value = '100%';
   imgUploadPreview.style.transform = 'scale(1)';
 
