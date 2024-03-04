c7460dd135a294ebe93dff7b165263271b3b71ef
Dkey51 <oniblekir@gmail.com>
2024-03-04 20:42:57 +0300

    правки для защиты

--- js/load.js
+++ js/load.js
@@ -4,15 +4,27 @@ const createErrorMessage = document.querySelector('#error').content.querySelecto
 const createSuccessMessage = document.querySelector('#success').content.querySelector('.success');
 const submitFormData = document.querySelector('.img-upload__submit');
 
-const showStatus = (status, templateMessage) => {
+let isStatusVisible = false;
+
+export const showStatus = (status, templateMessage) => {
 
   const notification = templateMessage.cloneNode(true);
+  isStatusVisible = true;
   const message = notification.querySelector(`.${status}__button`);
   document.body.appendChild(notification);
   message.addEventListener('click', ()=> {
     notification.remove();
+    isStatusVisible = false;
+  });
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      notification.remove();
+      isStatusVisible = false;
+    }
   });
 };
+
+export const getIsStatusVisible = () => isStatusVisible;
 
 export const getPosts = () => fetch(`${SERVER_ADDRESS}/data`)
   .then((response) => {
